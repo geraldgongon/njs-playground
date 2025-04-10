e79d7b88754fd8982a74c942222340bd54a25fe2
geraldgongon <geraldgongon@gmail.com>
2025-04-10 14:52:46 -0700

    debounce & throttle

--- app/lib/helpers/throttle.ts
+++ app/lib/helpers/throttle.ts
@@ -1,10 +1,15 @@
 /**
+ * Throttling limits the rate at which requests or data can be processed or transmitted,
+ * ensuring system stability and preventing overload, especially in large-scale, distributed
+ * environments.
+ * This function will only call the function once during any given wait time.
+ *
  * @callback func
- * @param {number} wait
+ * @param {number} wait time
  * @return {Function}
  */
 
-export default function throttle(func: Function, wait: number) {
+export default function throttle(func: Function, wait: number): Function {
   let timeout: string | number | NodeJS.Timeout | undefined;
   return function () {
     if (!timeout) {
